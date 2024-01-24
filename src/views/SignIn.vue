584dfe5a0035b0111814ddec21b0d619ba1856f1
Gandobilis <lashadeveloper@gmail.com>
2024-01-24 13:36:46 +0400

    absolute tag added.

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,27 +1,28 @@
 <script setup>
 import Side from '../components/SideBar.vue'
-import { useSignIn } from '../composables/signIn';
+import {useSignIn} from '../composables/signIn';
 
-const { inputs } = useSignIn();
-
+const {inputs} = useSignIn();
 </script>
 
 <template>
   <div class="flex">
-    <Side />
+    <Side/>
     <div class="flex w-1/2 justify-center">
       <div class="flex flex-col justify-center">
         <h2 class="text-5xl font-medium">ავტორიზაცია</h2>
-        <div class="flex flex-col gap-y-5">
+        <div class="flex flex-col gap-y-5 items-center">
           <div v-for="item in inputs" :key="item.index" class="flex flex-col mt-16">
             <input
-              type="text"
-              class="border border-black rounded-md w-60 h-12 placeholder:text-black placeholder:text-lg pl-3"
-              :placeholder="item.placeholder"
+                type="text"
+                class="border border-black rounded-md w-60 h-12 placeholder:text-black placeholder:text-lg pl-3"
+                :placeholder="item.placeholder"
             />
           </div>
-          <a href="" class="text-red text-lg flex justify-end">დაგავიწყდა ?</a>
-          <div>
+          <div class="relative">
+          <a href="" class="text-red text-lg absolute whitespace-nowrap">დაგავიწყდა?</a>
+          </div>
+          <div class="mt-10">
             <button class="px-20 py-2 bg-store-red text-lg rounded-xl text-white">შესვლა</button>
           </div>
           <div class="flex flex-col font-medium gap-y-4 items-center text-xl">
