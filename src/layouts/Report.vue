758994323d3792e5e55e1a41a28b055089bfd9f7
zurikelass <zuragogberashvili93@gmail.com>
2024-01-25 21:38:07 +0400

    changes

--- src/layouts/Report.vue
+++ src/layouts/Report.vue
@@ -6,13 +6,19 @@
     <div class="absolute text-black text-5xl font-normal mt-[30px] ml-[30px] font-['Noto Sans Georgian'] uppercase">დაარეპორტეთ ეს პოსტი</div>
     <div class="relative">
         <img
-     src="../svg/Vector.svg"
+     src="/src/logo/Vector.svg"
      class="absolute mt-[39px] right-[30px]   px-2 bg-white rounded-[5px]  "
     />
     </div>
    
         <div class="w-[876.02px] mt-[110px]  border border-stone-300"></div>
-        <div class="w-[876.02px] mt-[300px]  border border-stone-300"></div>
+        <div class="w-[339px] h-8 mt-[15px] ml-[30px] text-black text-2xl font-normal font-['Noto Sans Georgian']">აირჩიეთ რეპორტის მიზეზი</div>
+       
+        <div class="w-[192.42px] h-10 ml-[30px] mt-[30px] px-5 py-2.5 bg-red-800 rounded-[5px] shadow justify-center items-center gap-2.5 inline-flex">
+    <div class="text-white text-base font-normal font-['BPG Glaho'] tracking-wider">შევიწროვება</div>
+</div>
+
+        <!-- <div class="w-[876.02px] mt-[300px]  border border-stone-300"></div> -->
     
   </form>
 </div>
