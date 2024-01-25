77deacb0bfe9035a78a07c9955b15f47df5cbb57
Rikudo10 <giorgisamxaradze16@gmail.com>
2024-01-25 01:47:40 -0800

    layout and router

---
+++ src/layouts/mainLayout.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Footer from '../components/Footer.vue'
+import Header from '../components/Header.vue'
+</script>
+<template>
+    <Header />
+    <main class="container mx-auto">
+        <slot />
+    </main>
+    <Footer />
+</template>
---
+++ src/views/Pages/statementPage.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MainLayout from "@/layouts/mainLayout.vue"
+import
+</script>
+<template>
+      <main-layout>
+
+  </main-layout>
+</template>
