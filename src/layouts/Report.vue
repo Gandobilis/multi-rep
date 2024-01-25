9bb834a4b471e9daeb5a3a3a98594c672c8a1585
zurikelass <zuragogberashvili93@gmail.com>
2024-01-25 22:20:07 +0400

    make report first page

--- src/layouts/Report.vue
+++ src/layouts/Report.vue
@@ -11,14 +11,60 @@
     />
     </div>
    
-        <div class="w-[876.02px] mt-[110px]  border border-stone-300"></div>
+        <div class="w-[876.02px] mt-[110px]  border border-stone-300 "></div>
         <div class="w-[339px] h-8 mt-[15px] ml-[30px] text-black text-2xl font-normal font-['Noto Sans Georgian']">აირჩიეთ რეპორტის მიზეზი</div>
        
-        <div class="w-[192.42px] h-10 ml-[30px] mt-[30px] px-5 py-2.5 bg-red-800 rounded-[5px] shadow justify-center items-center gap-2.5 inline-flex">
+        <div class="w-[192.42px] h-10 ml-[30px] mt-[23px] px-5 py-2.5 bg-red-800 rounded-[5px] shadow justify-center items-center gap-2.5 inline-flex">
     <div class="text-white text-base font-normal font-['BPG Glaho'] tracking-wider">შევიწროვება</div>
+    <img
+    src="../logo/vector-plus.svg"/>
 </div>
-
-        <!-- <div class="w-[876.02px] mt-[300px]  border border-stone-300"></div> -->
+<div class="w-[291.32px] ml-[53.75px] mt-[23px] h-10 px-5 py-2.5 rounded-[5px] border border-zinc-300 justify-center items-center gap-2.5 inline-flex">
+    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">არასწორი ინფორმაცია</div>
+    <img
+    src="../logo/vector-plus.svg"/>
+</div>
+<div class="w-[225.75px] ml-[53.75px] h-10 px-5 py-2.5 rounded-[5px] border border-zinc-300 justify-center items-center gap-2.5 inline-flex">
+    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">ყალბი ანგარიში</div>
+    <img
+    src="../logo/vector-plus.svg"/>
+</div>
+<div class="w-[180.60px] ml-[30px] mt-[40px] h-10 px-5 py-2.5 rounded-[5px] border border-zinc-300 justify-center items-center gap-2.5 inline-flex">
+    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">თვით ზიანი</div>
+    <img
+    src="../logo/vector-plus.svg"/>
+</div>
+<div class="w-[193.50px] ml-[47.3px] mt-[30px] h-10 px-5 py-2.5 rounded-[5px] border border-zinc-300 justify-center items-center gap-2.5 inline-flex">
+    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">თაღლითობა</div>
+    <img
+    src="../logo/vector-plus.svg"/>
+</div>
+<div class="w-[122.55px] ml-[47.3px] mt-[30px] h-10 px-5 py-2.5 rounded-[5px] border border-zinc-300 justify-center items-center gap-2.5 inline-flex">
+    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">სპამი</div>
+    <img
+    src="../logo/vector-plus.svg"/>
+</div>
+<div class="w-[173.07px] ml-[47.3px] mt-[30px] h-10 px-5 py-2.5 rounded-[5px] border border-zinc-300 justify-center items-center gap-2.5 inline-flex">
+    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">ძალადობა</div>
+    <img
+    src="../logo/vector-plus.svg"/>
+</div>
+<div class="w-[280.57px] mt-[30px] ml-[30px] h-10 px-5 py-2.5 rounded-[5px] border border-zinc-300 justify-center items-center gap-2.5 inline-flex">
+    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">უკანონო მომსახურება</div>
+    <img
+    src="../logo/vector-plus.svg"/>
+</div>
+<div class="w-[193.50px] mt-[30px] ml-[30.1px] h-10 px-5 py-2.5 rounded-[5px] border border-zinc-300 justify-center items-center gap-2.5 inline-flex">
+    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">ცილისწამება</div>
+    <img
+    src="../logo/vector-plus.svg"/>
+</div>
+<div class="w-[282.72px] mt-[30px] ml-[30.1px] h-10 px-5 py-2.5 rounded-[5px] border border-zinc-300 justify-center items-center gap-2.5 inline-flex">
+    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider ">ბავშვის ექსპლუატაცია</div>
+    <img
+    src="../logo/vector-plus.svg"/>
+</div>
+        <div class="w-[876.02px] mt-[60px]  border "></div>
     
   </form>
 </div>
