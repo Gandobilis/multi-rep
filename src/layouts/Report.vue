6565ccc477c663ac890fc0e97d5bf8084dcc693c
Gandobilis <lashadeveloper@gmail.com>
2024-01-27 00:45:27 +0400

    reportTypes rendered dynamically.

--- src/layouts/Report.vue
+++ src/layouts/Report.vue
@@ -1,87 +1,122 @@
-<script setup
-
+<script setup>
+const reportTypes = [
+  ['შევიწროვება',
+    'არასწორი ინფორმაცია',
+    'ყალბი ანგარიში'],
+  ['თვითზიანი',
+    'თაღლითობა',
+    'სპამი',
+    'ძალადობა'],
+  ['უკანონო მომსახურება',
+    'ცილისწამებ',
+    'ბავშვის ექსპლუატაცია']
+];
 </script>
 <template>
   <div class="flex items-center justify-center h-screen">
-  <form class="w-[877px] h-[515px] bg-white rounded-[10px] border border-report1">
-    <div class="absolute text-black text-5xl font-normal mt-[30px] ml-[30px] font-['Noto Sans Georgian'] uppercase">დაარეპორტეთ ეს პოსტი</div>
-    <div class="relative">
+    <form class="w-[877px] h-[515px] bg-white rounded-[10px] border border-report1">
+      <div class="absolute text-black text-5xl font-normal mt-[30px] ml-[30px] font-['Noto Sans Georgian'] uppercase">
+        დაარეპორტეთ ეს პოსტი
+      </div>
+      <div class="relative">
         <img
-     src="/src/logo/Vector.svg"
-     class="absolute mt-[39px] right-[30px]   px-2 bg-white rounded-[5px]  "
-    />
-    </div>
-   
-        <div class="w-[876.02px] mt-[110px]  border border-report1 "></div>
-        <div class="w-[339px] h-8 mt-[15px] ml-[30px] text-black text-2xl font-normal font-['Noto Sans Georgian']">აირჩიეთ რეპორტის მიზეზი</div>
-       
-        <div class="w-[192.42px] h-10 ml-[30px] mt-[23px] px-5 py-2.5 bg-red-800 rounded-[5px] border border-report2 justify-center items-center gap-2.5 inline-flex">
-    <div class="text-white text-base font-normal font-['BPG Glaho'] tracking-wider">შევიწროვება</div>
-    <img
-    src="../logo/vector-plus.svg"/>
-</div>
-<div class="w-[291.32px] ml-[53.75px] mt-[23px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">
-    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">არასწორი ინფორმაცია</div>
-    <img
-    src="../logo/vector-plus.svg"/>
-</div>
-<div class="w-[225.75px] ml-[53.75px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">
-    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">ყალბი ანგარიში</div>
-    <img
-    src="../logo/vector-plus.svg"/>
-</div>
-<div class="w-[180.60px] ml-[30px] mt-[40px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">
-    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">თვითზიანი</div>
-    <img
-    src="../logo/vector-plus.svg"/>
-</div>
-<div class="w-[193.50px] ml-[47.3px] mt-[30px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">
-    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">თაღლითობა</div>
-    <img
-    src="../logo/vector-plus.svg"/>
-</div>
-<div class="w-[122.55px] ml-[47.3px] mt-[30px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">
-    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">სპამი</div>
-    <img
-    src="../logo/vector-plus.svg"/>
-</div>
-<div class="w-[173.07px] ml-[47.3px] mt-[30px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">
-    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">ძალადობა</div>
-    <img
-    src="../logo/vector-plus.svg"/>
-</div>
-<div class="w-[280.57px] mt-[30px] ml-[30px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">
-    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">უკანონო მომსახურება</div>
-    <img
-    src="../logo/vector-plus.svg"/>
-</div>
-<div class="w-[193.50px] mt-[30px] ml-[30.1px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">
-    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">ცილისწამება</div>
-    <img
-    src="../logo/vector-plus.svg"/>
-</div>
+            src="/src/logo/Vector.svg"
+            class="absolute mt-[39px] right-[30px]   px-2 bg-white rounded-[5px]  "
+        />
+      </div>
+
+      <div class="w-[876.02px] mt-[110px]  border border-report1 "></div>
+      <div class="w-[339px] h-8 mt-[15px] ml-[30px] text-black text-2xl font-normal font-['Noto Sans Georgian']">აირჩიეთ
+        რეპორტის მიზეზი
+      </div>
+
+      <div class="flex flex-col px-[30px] gap-y-[30px] pt-[23px] tracking-[1.6px]">
+        <div v-for="(row, index) in reportTypes" :key="index" class="w-full flex justify-between">
+          <div v-for="(item, itemIndex) in row" :key="itemIndex"
+               class="flex gap-x-[10px] py-[10px] px-5 border whitespace-nowrap border-report2 rounded-[5px] hover:bg-primary hover:text-white hover:cursor-pointer">
+            <p v-text="item"/>
+            <img src="../logo/vector-plus.svg" :alt="item"/>
+          </div>
+        </div>
+        <!--        <div v-for="(reportType, index) in reportTypes" :key="index"-->
+        <!--             class="flex gap-x-[10px] py-[10px] px-[5px] border whitespace-nowrap border-report2 rounded-[5px]">-->
+        <!--        </div>-->
+      </div>
+
+      <!--      <div-->
+      <!--          class="w-[192.42px] h-10 ml-[30px] mt-[23px] px-5 py-2.5 bg-red-800 rounded-[5px] border border-report2 justify-center items-center gap-2.5 inline-flex">-->
+      <!--        <div class="text-white text-base font-normal font-['BPG Glaho'] tracking-wider">შევიწროვება</div>-->
+      <!--        <img-->
+      <!--            src="../logo/vector-plus.svg"/>-->
+      <!--      </div>-->
+      <!--      <div-->
+      <!--          class="w-[291.32px] ml-[53.75px] mt-[23px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">-->
+      <!--        <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">არასწორი ინფორმაცია</div>-->
+      <!--        <img-->
+      <!--            src="../logo/vector-plus.svg"/>-->
+      <!--      </div>-->
+      <!--      <div-->
+      <!--          class="w-[225.75px] ml-[53.75px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">-->
+      <!--        <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">ყალბი ანგარიში</div>-->
+      <!--        <img-->
+      <!--            src="../logo/vector-plus.svg"/>-->
+      <!--      </div>-->
+      <!--      <div-->
+      <!--          class="w-[180.60px] ml-[30px] mt-[40px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">-->
+      <!--        <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">თვითზიანი</div>-->
+      <!--        <img-->
+      <!--            src="../logo/vector-plus.svg"/>-->
+      <!--      </div>-->
+      <!--      <div-->
+      <!--          class="w-[193.50px] ml-[47.3px] mt-[30px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">-->
+      <!--        <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">თაღლითობა</div>-->
+      <!--        <img-->
+      <!--            src="../logo/vector-plus.svg"/>-->
+      <!--      </div>-->
+      <!--      <div-->
+      <!--          class="w-[122.55px] ml-[47.3px] mt-[30px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">-->
+      <!--        <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">სპამი</div>-->
+      <!--        <img-->
+      <!--            src="../logo/vector-plus.svg"/>-->
+      <!--      </div>-->
+      <!--      <div-->
+      <!--          class="w-[173.07px] ml-[47.3px] mt-[30px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">-->
+      <!--        <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">ძალადობა</div>-->
+      <!--        <img-->
+      <!--            src="../logo/vector-plus.svg"/>-->
+      <!--      </div>-->
+      <!--      <div-->
+      <!--          class="w-[280.57px] mt-[30px] ml-[30px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">-->
+      <!--        <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">უკანონო მომსახურება</div>-->
+      <!--        <img-->
+      <!--            src="../logo/vector-plus.svg"/>-->
+      <!--      </div>-->
+      <!--      <div-->
+      <!--          class="w-[193.50px] mt-[30px] ml-[30.1px] h-10 px-5 py-2.5 rounded-[5px] border border-report2  justify-center items-center gap-2.5 inline-flex">-->
+      <!--        <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider">ცილისწამება</div>-->
+      <!--        <img-->
+      <!--            src="../logo/vector-plus.svg"/>-->
+      <!--      </div>-->
 
 
-    <button class="w-[282.72px] mt-[30px]  ml-[30.1px] h-10 px-5 py-2.5 hover:bg-primary rounded-[5px] border border-report2 justify-center items-center gap-2.5 inline-flex">
-    <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider ">ბავშვის ექსპლუატაცია</div>
-     
-     <img
-    
-    src="../logo/Vector(1).svg"
-    
-  /> 
-</button>
+      <!--      <button-->
+      <!--          class="w-[282.72px] mt-[30px]  ml-[30.1px] h-10 px-5 py-2.5 hover:bg-primary rounded-[5px] border border-report2 justify-center items-center gap-2.5 inline-flex">-->
+      <!--        <div class="text-black text-base font-normal font-['BPG Glaho'] tracking-wider ">ბავშვის ექსპლუატაცია</div>-->
+
+      <!--        <img-->
+
+      <!--            src="../logo/Vector(1).svg"-->
+
+      <!--        />-->
+      <!--      </button>-->
 
 
+      <div class="w-[876.02px] mt-[50px]  border border-report1 "></div>
+      <button
+          class="w-[200px] h-[35px] ml-[647px] mt-[30px] hover:bg-primary bg-secondary-bg rounded-[5px] justify-center items-center  inline-flex">
+        <div class="text-secondary-text text-base font-normal font-['Noto Sans Georgian']">რეპორტი</div>
+      </button>
+    </form>
+  </div>
 
-
- 
-
-        <div class="w-[876.02px] mt-[50px]  border border-report1 "></div>
-        <button class="w-[200px] h-[35px] ml-[647px] mt-[30px] hover:bg-primary bg-secondary-bg rounded-[5px] justify-center items-center  inline-flex">
-    <div class="text-secondary-text text-base font-normal font-['Noto Sans Georgian']">რეპორტი</div>
-</button>
-  </form>
-</div>
-    
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,18 @@ export default {
         "./src/**/*.{vue,js,ts,jsx,tsx}"
     ],
     theme: {
-        colors: {
-            'primary': '#950E1D',
-            'secondary-bg': '#FBACB5',
-            'secondary-text': '#FF96A2',
-            'meta': '#6F6F6F',
-            'star': '#FFE600',
-            'price': '#177B15',
-            'error': '#FF0000',
-            'report1':'#C9C9C9',
-            'report2':'#DBDBDB'
+        extend: {
+            colors: {
+                'primary': '#950E1D',
+                'secondary-bg': '#FBACB5',
+                'secondary-text': '#FF96A2',
+                'meta': '#6F6F6F',
+                'star': '#FFE600',
+                'price': '#177B15',
+                'error': '#FF0000',
+                'report1': '#C9C9C9',
+                'report2': '#DBDBDB'
+            }
         }
     }
 }
