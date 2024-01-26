db3c2ef2e063aac29a703b7aabe945bd7e22a8ad
xvedela <dato.xvedelidze.888@gmail.com>
2024-01-27 01:48:14 +0400

    design all fixed and is redy

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,17 +5,18 @@ import search from '../assests/icons/auth/search.vue'
 import man from '../assests/icons/auth/man.vue'
 </script>
 <template>
-  <div class="flex pt-10 px-20 justify-between">
-    <div class="flex items-center">
-      <mainLogo class="mr-4"/>
-      <h1 class="text-primary text-2xl font-medium tracking-wide">E-LEARNING</h1>
+    <div class="flex pt-10 px-20 justify-between">
+        <div class="flex items-center">
+            <mainLogo class="mr-4" />
+            <h1 class="text-primary text-2xl font-medium ">E-LEARNING</h1>
+        </div>
+        <div class="flex">
+            <p class="mr-10 text-xl">მთავარი</p>
+            <p class="mr-20 text-xl">ლიდერბორდი</p>
+            <heart />
+            <man class="mx-20" />
+            <search />
+        </div>
     </div>
-    <div class="flex">
-      <p class="mr-10 text-xl">მთავარი</p>
-      <p class="mr-20 text-xl">ლიდერბორდი</p>
-      <heart/>
-      <man class="mx-20"/>
-      <search/>
-    </div>
-  </div>
+    <hr class="border-t border-gray-300 my-8 w-full" />
 </template>
