2ade7bb444db572ecb362905524df8f637731f08
Gandobilis <lashadeveloper@gmail.com>
2024-01-27 15:32:06 +0400

    auth login page final version.

--- src/views/auth/login/LoginView.vue
+++ src/views/auth/login/LoginView.vue
@@ -17,16 +17,15 @@ const authorize = () => {
 </script>
 <template>
   <form action="" method="post" @submit.prevent="authorize">
-    <h1 class="text-4xl uppercase mb-[50px] tracking-wide">ავტორიზაცია</h1>
+    <h1 class="text-[32px] mb-[50px] font-semibold">ავტორიზაცია</h1>
     <div class="relative w-full mb-[50px]">
       <input
           v-model="email"
-          class="placeholder-black focus:outline-primary border-black border rounded-[5px] w-full pt-[7px] pb-[6px] px-2.5"
+          class="placeholder-black focus:outline-primary border-black border rounded-[5px] w-full pt-[7px] pb-1.5 px-2.5"
           type="email"/>
       <transition>
         <label
-            class="absolute -top-[14px] left-[8px] px-[4px] bg-white">ელ.
-          ფოსტა</label>
+            class="absolute -top-3.5 left-2.5 px-1 bg-white">ელ. ფოსტა</label>
       </transition>
     </div>
     <div class="relative w-full mb-5">
@@ -34,14 +33,14 @@ const authorize = () => {
            :src="show ? '/src/assets/auth/hide-password-icon.svg' : '/src/assets/auth/show-password-icon.svg'" alt="">
       <input
           v-model="password"
-          class="placeholder-black focus:outline-primary border-black border rounded-[5px] w-full pt-[7px] pb-[6px] px-2.5"
+          class="placeholder-black focus:outline-primary border-black border rounded-[5px] w-full pt-[7px] pb-1.5 px-2.5"
           :type="show ? 'text' : 'password'">
       <transition>
-        <label class="absolute -top-[14px] left-[8px] px-[4px] bg-white">პაროლი</label>
+        <label class="absolute -top-3.5 left-2.5 px-1 bg-white">პაროლი</label>
       </transition>
     </div>
-    <router-link class="flex w-full justify-end  text-[#950E1D] text-xs mb-5" to="">დაგავიწყდა?</router-link>
-    <button class="bg-primary text-white py-2 rounded w-full hover:shadow-2xl mb-[15px]">შესვლა</button>
+    <router-link class="flex w-full justify-end  text-primary text-xs mb-5" to="">დაგავიწყდა?</router-link>
+    <button class="bg-primary text-white py-[6.5px] rounded w-full hover:shadow-2xl mb-[15px]">შესვლა</button>
     <h3 class="flex justify-center mb-[15px]"> ან</h3>
     <button class="text-primary  py-[6.5px] rounded-[5px] hover:shadow-2xl hover:text-white hover:bg-primary w-full">
       რეგისტრაცია
