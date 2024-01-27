0e0930ae27b4ff3e2618e419759e972013180265
ana94-ana <aniabramishvili94@gmail.com>
2024-01-27 20:26:08 +0400

    remove filter stile

--- src/components/courses/Filter.vue
+++ src/components/courses/Filter.vue
@@ -17,16 +17,16 @@ const show = ref(false)
             <!-- filter menu -->
             <ul v-show="show"
                 class=" absolute  right-0 py-2 mr-12  mt-2 bg-indigo-500 rounded-md border-[#CACACA] shadow-xl w-60 ">
-                <li class=" block px-4 py-2 text-sm text-indigo-100  hover:bg-indigo-400 hover:text-indigo-100 hover:cursor-pointer">
+                <li class=" block px-4 py-2 text-sm  hover:cursor-pointer">
                     პოპულარული
                 </li>
-                <li class=" block px-4 py-2 text-sm text-indigo-100  hover:bg-indigo-400 hover:text-indigo-100 hover:cursor-pointer">
+                <li class=" block px-4 py-2 text-sm  hover:cursor-pointer">
                     ახლანდელი
                 </li>
-                <li class=" block px-4 py-2 text-sm text-indigo-100  hover:bg-indigo-400 hover:text-indigo-100 hover:cursor-pointer">
+                <li class=" block px-4 py-2 text-sm  hover:cursor-pointer">
                     პროგრამირება
                 </li>
-                <li class=" block px-4 py-2 text-sm text-indigo-100  hover:bg-indigo-400 hover:text-indigo-100 hover:cursor-pointer">
+                <li class=" block px-4 py-2 text-sm  hover:cursor-pointer">
                     ზოგადი
                 </li>
             </ul>
