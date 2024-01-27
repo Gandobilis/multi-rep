5dde0354c3a6f712b7046ef4e08d00e83026fa7f
Gandobilis <lashadeveloper@gmail.com>
2024-01-27 21:54:54 +0400

    some updated added.

--- src/components/courses/Filter.vue
+++ src/components/courses/Filter.vue
@@ -1,36 +1,36 @@
 <script setup>
-import { ref } from "vue"
-const show = ref(false)
+import {ref} from "vue"
 
+const show = ref(false)
 </script>
 <template>
+  <div>
     <div>
-        <div>
-            <!-- filter  button -->
-            <div class="flex flex-row space-x-1">
-                <p class="">დალაგება:</p>
-                <div class="flex flex-row space-x-1" @click="show = !show">
-                    <button>პოპულარული</button>
-                    <img class="hover:cursor-pointer" src="../../icone/choiBox.svg" alt="">
-                </div>
-            </div>
-            <!-- filter menu -->
-            <ul v-show="show"
-                class=" absolute  right-0 py-2 mr-12  mt-2 bg-indigo-500 rounded-md border-[#CACACA] shadow-xl w-60 ">
-                <li class=" block px-4 py-2 text-sm  hover:cursor-pointer">
-                    პოპულარული
-                </li>
-                <li class=" block px-4 py-2 text-sm  hover:cursor-pointer">
-                    ახლანდელი
-                </li>
-                <li class=" block px-4 py-2 text-sm  hover:cursor-pointer">
-                    პროგრამირება
-                </li>
-                <li class=" block px-4 py-2 text-sm  hover:cursor-pointer">
-                    ზოგადი
-                </li>
-            </ul>
+      <!-- filter  button -->
+      <div class="flex flex-row space-x-1">
+        <p class="">დალაგება:</p>
+        <div class="flex flex-row space-x-1" @click="show = !show">
+          <button>პოპულარული</button>
+          <img class="hover:cursor-pointer" src="../../icone/choiBox.svg" alt="">
         </div>
+      </div>
+      <!-- filter menu -->
+      <ul v-show="show"
+          class="absolute z-10 bg-white right-0 py-2 mr-12  mt-2 rounded-md border-[#CACACA] shadow-xl w-60 ">
+        <li class=" block px-4 py-2 text-sm  hover:cursor-pointer">
+          პოპულარული
+        </li>
+        <li class=" block px-4 py-2 text-sm  hover:cursor-pointer">
+          ახლანდელი
+        </li>
+        <li class=" block px-4 py-2 text-sm  hover:cursor-pointer">
+          პროგრამირება
+        </li>
+        <li class=" block px-4 py-2 text-sm  hover:cursor-pointer">
+          ზოგადი
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
  
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,16 @@ export default {
         "./src/**/*.{vue,js,ts,jsx,tsx}"
     ],
     theme: {
-        colors: {
-            'primary': '#950E1D',
-            'secondary-bg': '#FBACB5',
-            'secondary-text': '#FF96A2',
-            'meta': '#6F6F6F',
-            'star': '#FFE600',
-            'price': '#177B15',
-            'error': '#FF0000'
+        extend: {
+            colors: {
+                'primary': '#950E1D',
+                'secondary-bg': '#FBACB5',
+                'secondary-text': '#FF96A2',
+                'meta': '#6F6F6F',
+                'star': '#FFE600',
+                'price': '#177B15',
+                'error': '#FF0000'
+            },
         }
     }
 }
