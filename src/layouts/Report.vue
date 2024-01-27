98f1739d94f894c9066169341ac8ff860427356f
Gandobilis <lashadeveloper@gmail.com>
2024-01-27 20:43:08 +0400

    white plus icon rendered on hover.

--- src/layouts/Report.vue
+++ src/layouts/Report.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 
+import {ref} from "vue";
 
 const reportTypes = [
   ['შევიწროვება',
@@ -14,52 +15,70 @@ const reportTypes = [
     'ცილისწამება',
     'ბავშვის ექსპლუატაცია']
 ];
-const items= [
+const items = [
   'დაარეპორტეთ ეს პოსტი',
 ];
-const reasons=[
+
+const _hoverIndex = ref('')
+const _clickIndex = ref('')
+const hoveredIndex = (i, j) => {
+  _hoverIndex.value = i.toString() + j.toString()
+}
+
+const clickedIndex = (i, j) => {
+  clickedIndex.value = i.toString() + j.toString()
+}
+
+const clearIndex = () => {
+  _hoverIndex.value = ''
+}
+
+const reasons = [
   " აირჩიეთ რეპორტის მიზეზი",
 ];
-
 
 </script>
 <template>
   <div class="flex items-center justify-center h-screen">
     <form class="w-[877px] h-[515px] bg-white rounded-[10px] border border-report1">
-      <div v-for="(item) in items" class=" absolute text-5xl font-normal mt-[30px] ml-[30px] font-['Noto Sans Georgian'] uppercase">
-     {{item}}
-  </div>
+      <div v-for="(item) in items"
+           class=" absolute text-5xl font-normal mt-[30px] ml-[30px] font-['Noto Sans Georgian'] uppercase"
+           v-text="item"/>
       <div class="relative">
         <img
             src="/src/logo/Vector.svg"
-            class="absolute mt-[39px] right-[30px]   px-2 bg-white rounded-[5px]  "
+            class="absolute mt-[39px] right-[30px]   px-2 bg-white rounded-[5px]"
+            alt="add icon"
         />
       </div>
       <div class="w-[876.02px] mt-[110px]  border border-report1 "></div>
       <!-- მიზეზის არჩევა -->
-      <div v-for="(reason) in reasons" class="w-[339px] h-8 mt-[15px] ml-[30px] text-black text-2xl font-['Noto Sans Georgian']" >
-      {{ reason }}</div>
-    
-<!-- მიზეზები -->
+      <div v-for="(reason) in reasons"
+           class="w-[339px] h-8 mt-[15px] ml-[30px] text-2xl" v-text="reason"/>
+
+      <!-- მიზეზები -->
       <div class="flex flex-col px-[30px] gap-y-[30px] pt-[23px] tracking-[1.6px]">
-        <div  v-for="(row, index) in reportTypes" :key="index" class="w-full flex justify-between">
-          <div  v-for="(item, itemIndex) in row" :key="itemIndex"
+        <div v-for="(row, index) in reportTypes" :key="index" class="w-full flex justify-between">
+          <div v-for="(item, itemIndex) in row" :key="itemIndex"
+               @mouseover="hoveredIndex(row, item)"
+               @mouseout="clearIndex"
+               @click="clickedIndex(row, item)"
                class="flex gap-x-[10px] py-[10px] px-5 border whitespace-nowrap border-report2 rounded-[5px] hover:bg-primary hover:text-white hover:cursor-pointer">
             <p v-text="item"/>
-            
-            <img src="../logo/vector-plus.svg" :alt="item"
-              
-             />
+
+            <img
+                :src="row.toString() + item.toString() === _hoverIndex ? 'src/logo/Vector-white.svg' : 'src/logo/vector-plus.svg'"
+                :alt="item"
+            />
           </div>
         </div>
       </div>
-   
 
       <div class="w-[876.02px] mt-[50px]  border border-report1 "></div>
 
       <button
-          class="w-[200px] h-[35px] ml-[647px] mt-[30px] hover:bg-primary bg-secondary-bg rounded-[5px] justify-center items-center  inline-flex">
-        <div class="text-secondary-text text-base font-normal font-['Noto Sans Georgian']">რეპორტი</div>
+          class="px-[60.5px] py-[6.5px] ml-[647px] mt-[30px] text-secondary-text bg-secondary-bg rounded-[5px]">
+        რეპორტი
       </button>
     </form>
   </div>
