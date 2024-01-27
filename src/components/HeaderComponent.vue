97f189a73e0b455b81a6ec45e3df883a29ab4842
xvedela <dato.xvedelidze.888@gmail.com>
2024-01-27 20:58:10 +0400

    added underline text and hover

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,12 +1,16 @@
 <script setup>
-import mainLogo from '../assests/icons/auth/main-logo.vue'
-import heart from '../assests/icons/auth/heart.vue'
-import search from '../assests/icons/auth/search.vue'
-import man from '../assests/icons/auth/man.vue'
-import exit from '../assests/icons/auth/exit.vue'
+import mainLogo from '../assests/icons/auth/main-logo.vue';
+import heart from '../assests/icons/auth/heart.vue';
+import search from '../assests/icons/auth/search.vue';
+import man from '../assests/icons/auth/man.vue';
+import exit from '../assests/icons/auth/exit.vue';
+import { ref } from 'vue';
 
+const activeTab = ref(null);
 
-
+const handleClick = (tab) => {
+    activeTab.value = tab;
+};
 </script>
 <template>
     <div class="flex pt-10 px-20 justify-between">
@@ -15,15 +19,23 @@ import exit from '../assests/icons/auth/exit.vue'
             <h1 class="text-primary text-2xl font-medium ">E-LEARNING</h1>
         </div>
         <div class="flex">
-            <button
-                class="mr-10 text-xl relative focus:outline-none hover:text-red-500 hover:border-b-2 hover:border-red-500"
-                onclick="handleClick('first')">მთავარი</button>
-            <button
-                class="mr-20 text-xl relative focus:outline-none hover:text-red-500 hover:border-b-2 hover:border-red-500"
-                onclick="handleClick('second')">ლიდერბორდი</button>
+            <button class="mr-10 text-xl relative focus:outline-none 
+        hover:text-red-500 hover:border-b-2 hover:border-red-500 
+        focus:text-red-500 focus:border-b-2 focus:border-red-500 
+        {{ activeTab === 'first' ? 'underline' : '' }}" @click="handleClick('first')">მთავარი
+            </button>
+            <button class="mr-20 text-xl relative focus:outline-none 
+        hover:text-red-500 hover:border-b-2 hover:border-red-500 
+        focus:text-red-500 focus:border-b-2 focus:border-red-500 
+        {{ activeTab === 'second' ? 'underline' : '' }}" @click="handleClick('second')">ლიდერბორდი
+            </button>
             <heart />
             <man class="mx-20" />
             <search />
         </div>
     </div>
-<hr class="border-t border-gray-300 my-8 w-full" /></template>
+    <hr class="border-t border-gray-300 my-8 w-full" />
+</template>
+
+
+
