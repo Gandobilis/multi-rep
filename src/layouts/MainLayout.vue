feb1ce813b861bbcba180db57c7c10b899ac52f7
Gandobilis <lashadeveloper@gmail.com>
2024-01-29 19:53:48 +0400

    missing semicolons and colons.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,6 @@
 import HeaderComponent from "../components/layouts/main/HeaderComponent.vue";
 import FooterComponent from "../components/layouts/main/FooterComponent.vue";
 </script>
-
 <template>
   <header-component/>
   <main>
