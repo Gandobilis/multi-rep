a7fefbe1db8c0f240d1ee4f28a93889ab332acac
zurikelass <zuragogberashvili93@gmail.com>
2024-01-29 23:45:26 +0400

    makes radio ring?????

--- src/views/auth/AddPost.vue
+++ src/views/auth/AddPost.vue
@@ -2,18 +2,19 @@
 <template>
   <div class="flex items-center justify-center h-screen">
     <form class="w-[877px] h-[730px] rounded-[10px] border border-stone-300">
-      <h1 class="absolute text-5xl mt-[30px] ml-[40px] uppercase">
+      <h1 class="absolute text-5xl mt-[30px] ml-[30px] uppercase">
         პოსტის დამატება
       </h1>
       <div class="relative">
         <img
           src="/src/logo/Vector.svg"
-          class="absolute mt-[49px] right-[40px] px-2 bg-white rounded-[5px]"
+          class="absolute mt-[49px] right-[30px]  bg-white rounded-[5px]"
           alt="add icon"
         />
       </div>
-      <div class="w-[876.02px] mt-[110px] border border-report1"></div>
+      <div class="w-[876.02px] mt-[100px] border border-report1"></div>
       <div class="flex flex-col justify-center items-center">
+      <div></div>
         <input
           class="mt-[30px] w-[300px] h-[35px] placeholder-black rounded-[5px] border border-black input-field"
           placeholder="სათაური "
@@ -30,7 +31,7 @@
               type="radio"
               value=""
               name="default-radio"
-              class="ml-[10px] mt-[13px] w-2.5 h-2.5 "
+              class="ring-offset-green-400 ml-[10px] mt-[13px] w-2.5 h-2.5 "
             />
             <label
               for="default-radio-1"
