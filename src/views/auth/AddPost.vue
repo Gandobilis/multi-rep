f2e520f50736668c53ec81eeb04e62f5b63a6167
Gandobilis <lashadeveloper@gmail.com>
2024-01-29 20:38:27 +0400

    პოსტის დამატების ფორმის სტატიკური რენდერი.

--- src/views/auth/AddPost.vue
+++ src/views/auth/AddPost.vue
@@ -1,28 +1,15 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import {ref, onMounted} from 'vue';
 
-const heads = ref ('სათაური')
+const heads = ref('სათაური')
 onMounted(() => {
-  heads.value.focus() });
-  
-  const items =[
-    'პოსტის დამატება'
-];
-
-
-const abouts = ref ('აღწერა')
-onMounted(() => {
-  abouts.value.focus() });
-  
+  heads.value.focus()
+});
 </script>
 <template>
-  <div  class="flex items-center justify-center h-screen">
-    <form class="w-[877px] h-[730px] rounded-[10px] border border-stone-300"> 
-    
-    
-      <div v-for="(item) in items"
-           class=" absolute text-5xl  mt-[30px] ml-[40px] uppercase"
-           v-text="item"/>
+  <div class="flex items-center justify-center h-screen">
+    <form class="w-[877px] h-[730px] rounded-[10px] border border-stone-300">
+      <h1 class=" absolute text-5xl  mt-[30px] ml-[40px] uppercase">პოსტის დამატება</h1>
       <div class="relative">
         <img
             src="/src/logo/Vector.svg"
@@ -30,54 +17,27 @@ onMounted(() => {
             alt="add icon"
         />
       </div>
-    
-    
-    <!-- xazi -->
-      
       <div class="w-[876.02px] mt-[110px]  border border-report1 "></div>
-
- <!-- xazi -->
-  
- <div class="flex flex-col justify-center  items-center">
-  
-    
-  
-  <input ref="heads" class="mt-[30px] w-[300px] h-[35px]  rounded-[5px] border border-black input-field" placeholder="სათაური" />
-
-   <input ref="abouts"  class="w-[300px] h-[150px] mt-[30px] rounded-[5px] border border-black" placeholder="აღწერა"/>
-   
-    <div class="mt-[30px] w-[300px] h-[70px] rounded-[5px] border border-black">
-     
-<div class="flex items-center mb-4">
-    <input  type="radio" value="" name="default-radio" class="ml-[10px] mt-[13px] w-2.5 h-2.5 text-blue-600">
-    <label for="default-radio-1" class="text-black ml-[10px] text-xs mt-[13px] ">ზოგადი</label>
-</div>
-<div class="flex items-center">
-    <input  type="radio" value="" name="default-radio" class="  ml-[10px] mb-[13px] w-2.5 h-2.5 text-blue-600  ">
-    <label for="default-radio-2" class="mb-[13px] ml-[10px] text-black text-xs ">კურსი</label>
-</div>
-  
-    </div>
-  
-   <div class="flex flex-cols" >
-      <p class="w-[190px] h-[35px] rounded-[5px] border border-black" >ხანგრძლივობა</p>
-      <p class="w-[90px]  h-[35px]  rounded-[5px] border border-black">xzcsc</p>
-     </div>
-     
-    
-
-
-  
-
-</div>
-
-    
-  
-
-
-
-    
-
+      <div class="flex flex-col justify-center  items-center">
+        <input ref="heads" class="mt-[30px] w-[300px] h-[35px]  rounded-[5px] border border-black input-field"
+               placeholder="სათაური"/>
+        <input ref="abouts" class="w-[300px] h-[150px] mt-[30px] rounded-[5px] border border-black"
+               placeholder="აღწერა"/>
+        <div class="mt-[30px] w-[300px] h-[70px] rounded-[5px] border border-black">
+          <div class="flex items-center mb-4">
+            <input type="radio" value="" name="default-radio" class="ml-[10px] mt-[13px] w-2.5 h-2.5 text-blue-600">
+            <label for="default-radio-1" class="text-black ml-[10px] text-xs mt-[13px] ">ზოგადი</label>
+          </div>
+          <div class="flex items-center">
+            <input type="radio" value="" name="default-radio" class="  ml-[10px] mb-[13px] w-2.5 h-2.5 text-blue-600  ">
+            <label for="default-radio-2" class="mb-[13px] ml-[10px] text-black text-xs ">კურსი</label>
+          </div>
+        </div>
+        <div class="flex flex-cols">
+          <p class="w-[190px] h-[35px] rounded-[5px] border border-black">ხანგრძლივობა</p>
+          <p class="w-[90px]  h-[35px]  rounded-[5px] border border-black">xzcsc</p>
+        </div>
+      </div>
     </form>
   </div>
 </template>
