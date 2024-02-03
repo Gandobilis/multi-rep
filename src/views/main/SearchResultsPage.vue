6d493e7ff6633cfadcc6da9e3c78923c41f68505
Rikudo10 <giorgisamxaradze16@gmail.com>
2024-02-02 17:33:39 -0800

    searchresults

---
+++ src/views/main/SearchResultsPage.vue
@@ -0,0 +1,8 @@
+<script setup>
+import SearchResults from '../../components/layouts/main/SearchResults/SearchResults.vue';
+</script>
+<template>
+    <div class="container px-12">
+        <SearchResults />
+    </div>
+</template>
