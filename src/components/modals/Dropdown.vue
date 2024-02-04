54799ebe4f3a98f57c6704b3fee88eadd074ee32
Gandobilis <lashadeveloper@gmail.com>
2024-02-04 21:59:20 +0400

    Dropdown.vue formatted with tailwind formatter.

--- src/components/modals/Dropdown.vue
+++ src/components/modals/Dropdown.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="grid grid-cols-2 gap-x-4">
-
     <animated-input v-model="value" :placeholder="label"/>
-
     <div
         class="relative bg-[#ECECEC] rounded-md border border-black font-medium p-2.5">
-
-      <div class="cursor-pointer flex justify-between items-center" @click="emit('toggleDropdown')">
+      <div class="flex cursor-pointer items-center justify-between" @click="emit('toggleDropdown')">
         <p v-text="model"/>
         <img :class="{'rotate-180': show}" src="/src/assets/icons/modals/down-arrow.svg" alt="arrow icon"/>
       </div>
-
       <div
           v-if="show"
-          class="z-10 absolute left-0 flex w-full flex-col rounded-md border border-black bg-white top-[50px] p-2.5 gap-y-2.5">
+          class="absolute left-0 z-10 flex w-full flex-col rounded-md border border-black bg-white top-[50px] p-2.5 gap-y-2.5">
         <p v-for="(unit, index) in options" :class="{'font-semibold text-primary': unit === model}"
            class="cursor-pointer"
            :key="index" @click="model = unit;emit('closeDropDown')" v-text="unit"/>
@@ -25,23 +21,13 @@
 <script setup>
 import AnimatedInput from "@/components/layouts/auth/AnimatedInput.vue";
 
-const emit = defineEmits(['closeDropDown', 'toggleDropdown']);
-
-defineProps({
-  label: {
-    type: String,
-    required: true
-  },
-  show: {
-    type: Boolean,
-    required: true
-  },
-  options: {
-    type: Object,
-    required: true
-  }
-})
-
 const model = defineModel('unit')
 const value = defineModel('value')
+
+const emit = defineEmits(['closeDropDown', 'toggleDropdown']);
+defineProps({
+  label: {type: String, required: true},
+  show: {type: Boolean, required: true},
+  options: {type: Object, required: true}
+})
 </script>
