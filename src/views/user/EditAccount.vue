150ddc7b01b2a63fc7497bcd9bfa6e1db4fe5fd1
Gandobilis <lashadeveloper@gmail.com>
2024-02-06 12:31:32 +0400

    მომხმარებლის გვერდი: ანგარიშის რედაქტირება. (v-for)

--- src/views/user/EditAccount.vue
+++ src/views/user/EditAccount.vue
@@ -3,47 +3,27 @@ import useUser from "../../composables/useUser.js";
 import AnimatedInput from "../../components/auth/AnimatedInput.vue";
 
 const {editAccount} = useUser();
+
+const formFields = [
+  {label: 'სახელი:', model: 'first_name'},
+  {label: 'გვარი:', model: 'last_name'},
+  {label: 'ტელ. ნომერი:', model: 'phone'},
+  {label: 'ქალაქი:', model: 'city'},
+  {label: 'პროფესია:', model: 'profession'}
+];
 </script>
 
 <template>
   <form @submit.prevent="" class="h-full flex justify-center items-center">
     <div class="flex flex-col justify-between gap-y-10">
-      <div class="grid grid-cols-3 items-center gap-x-1">
-        <label class="whitespace-nowrap" v-text="'სახელი:'"/>
+      <div v-for="(field, index) in formFields" :key="index" class="grid grid-cols-3 items-center gap-x-1">
+        <label class="whitespace-nowrap" :for="field.model" v-text="field.label"/>
 
-        <animated-input class="col-span-2" v-model="editAccount.first_name"/>
-      </div>
+        <div class="relative col-span-2">
+          <animated-input v-model="editAccount[field.model]" :id="field.model"/>
 
-      <div class="grid grid-cols-3 items-center gap-x-1">
-        <label class="whitespace-nowrap" v-text="'გვარი:'"/>
-
-        <animated-input class="col-span-2" v-model="editAccount.last_name"/>
-      </div>
-
-      <div class="grid grid-cols-3 items-center gap-x-1">
-        <label class="whitespace-nowrap" v-text="'ტელ. ნომერი:'"/>
-
-        <animated-input class="col-span-2" v-model="editAccount.phone"/>
-      </div>
-
-      <div class="grid grid-cols-3 items-center gap-x-1">
-        <label class="whitespace-nowrap" v-text="'ქალაქი:'"/>
-
-        <div class="relative">
-          <animated-input class="col-span-2" v-model="editAccount.city"/>
-
-          <img class="absolute top-[15px] right-[15px] hover:cursor-pointer" src="/src/assets/icons/user/close.svg"
-               alt="close icon"/>
-        </div>
-      </div>
-
-      <div class="grid grid-cols-3 items-center gap-x-1">
-        <label class="whitespace-nowrap" v-text="'პროფესია:'"/>
-
-        <div class="relative">
-          <animated-input class="col-span-2" v-model="editAccount.profession"/>
-
-          <img class="absolute top-[15px] right-[15px] hover:cursor-pointer" src="/src/assets/icons/user/close.svg"
+          <img v-if="['city', 'profession'].includes(field.model)"
+               class="absolute top-[15px] right-[15px] hover:cursor-pointer" src="/src/assets/icons/user/close.svg"
                alt="close icon"/>
         </div>
       </div>
