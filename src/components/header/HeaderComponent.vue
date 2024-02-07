553f21f4a596c350e75857d4351de8076af4041d
Gandobilis <lashadeveloper@gmail.com>
2024-02-08 01:22:38 +0400

    ჰედერი: შეტყობინებების სიმბოლო, თუ მასწავლებელია.

--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -1,8 +1,12 @@
 <script setup>
-import NavLink from "/src/components/header/NavLink.vue";
-import Favourite from "/src/components/header/Favourite.vue";
-import User from "/src/components/header/User.vue";
-import Search from "/src/components/header/Search.vue";
+import NavLink from "/src/components/header/NavLink.vue"
+import Favourite from "/src/components/header/Favourite.vue"
+import User from "/src/components/header/User.vue"
+import Search from "/src/components/header/Search.vue"
+import {ref} from "vue"
+import Notifications from "./Notifications.vue"
+
+const is_teacher = ref(true)
 </script>
 <template>
   <nav class="flex justify-between items-center">
@@ -14,6 +18,7 @@ import Search from "/src/components/header/Search.vue";
     <div class="flex items-center gap-x-24">
       <nav-link path="/">მთავარი</nav-link>
       <nav-link path="/leaderboard">ლიდერბორდი</nav-link>
+      <notifications v-if="is_teacher"/>
       <favourite/>
       <user/>
       <search/>
---
+++ src/components/header/Notifications.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {ref} from "vue"
+import NotificationsIcon from "../../assets/icons/header/NotificationsIcon.vue"
+
+const show = ref(false)
+const favourites = ref(1)
+const statements = ref([
+  'ახალი ჯავას კურსი',
+  'ახალი ჯავას კურსი',
+  'ახალი ჯავას კურსი'
+])
+</script>
+
+<template>
+  <div class="relative">
+    <div class="cursor-pointer">
+      <notifications-icon @click="show = !show" :stroke="show ? '#950E1D' : 'black'"/>
+      <span
+          :class="show ? 'text-primary border border-primary  bg-white' : 'bg-primary text-white'"
+          class="-top-2.5 -right-2.5 absolute w-6 h-6 rounded-full flex items-center justify-center"
+          v-text="favourites"/>
+    </div>
+    <div class="absolute bg-white z-10 border border-gray-400 rounded-md top-12 right-0"
+         :class="show ? 'block' : 'hidden'">
+      <p class="whitespace-nowrap text-sm font-bold p-3">შენახული განცხადები</p>
+      <div class="flex flex-col border-t border-t-gray-400 p-3 gap-y-2">
+        <router-link
+            to="/"
+            @click="show = !show"
+            v-for="(statement, index) in statements" :key="index"
+            class="whitespace-nowrap cursor-pointer"
+            v-text="statement"/>
+      </div>
+    </div>
+  </div>
+</template>
