6ee40b60d5152fcbab6e8f0a5d1961be5609818e
wpapu <wpapuna5@gmail.com>
2024-02-12 02:28:42 +0400

    F

--- src/components/statementComp/similarListing.vue
+++ src/components/statementComp/similarListing.vue
@@ -20,13 +20,13 @@ import Star from "../../assets/icons/SearchResults/star.vue";
         <span>{{props.data.average_listing_score}}</span>/ 5
         <star/>
       </div>
-      <p class="text-sm text-ce">{{props.data.description?.length > 80
-          ? props.data.description.slice(0, 80) + "..."
+      <p class="text-sm text-ce">{{props.data.description?.length > 60
+          ? props.data.description.slice(0, 60) + "..."
           : props.data.description}}</p>
 
       <div class="flex gap-3 border-2 py-1 px-3 justify-center rounded-3xl border-secondary-bg items-center text-sm font-medium">
-        <img class="w-10 rounded-full h-10" src="https://servisebi.ge/upload/product/thumbs/5/17532_1.jpg?v=41" alt="">
-        <p>ანდრო</p>
+        <img class="w-10 rounded-full h-10" :src="props.data.teacher.profile_pic" alt="">
+        <p>{{props.data.teacher.name}}</p>
         <p class="bg-primary text-white py-1 px-3 rounded-2xl text-center">598757596</p>
       </div>
     </div>
--- src/components/statementComp/similarListings.vue
+++ src/components/statementComp/similarListings.vue
@@ -6,9 +6,9 @@ const {similarListingsForSpecificListing } = useStatement();
 </script>
 
 <template>
-  <div class="flex flex-col gap-8 mt-8">
+  <div class="flex flex-col gap-10 mt-8">
     <p class="font-bold text-3xl">მსგავსი განცხადებები</p>
-    <div class="grid gap-5 grid-cols-4">
+    <div class="grid gap-20 grid-cols-4">
       <SimilarListing v-if="similarListingsForSpecificListing" v-for="listing in similarListingsForSpecificListing" :data="listing"/>
     </div>
   </div>
