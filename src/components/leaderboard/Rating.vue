80eb69bbef745ad27c7fd4120596bfa61eb217f8
Gandobilis <lashadeveloper@gmail.com>
2024-02-20 00:42:41 +0400

    Ratings.vue

--- src/components/leaderboard/Rating.vue
+++ src/components/leaderboard/Rating.vue
@@ -10,16 +10,24 @@ defineProps({
 </script>
 
 <template>
-  <div class="grid grid-rows-3 grid-cols-3 lg:grid-rows-1 lg:grid-cols-7 items-center font-medium text-sm lg:text-xl border border-gray-400 px-4 pt-3 lg:py-6 rounded-lg">
+  <div
+      class="grid grid-rows-3 grid-cols-3 lg:grid-rows-1 lg:grid-cols-7 items-center font-medium text-sm lg:text-xl border border-gray-400 px-4 pt-3 lg:py-6 rounded-lg">
     <img class="max-lg:w-14" :src="rating.image" alt="rating image">
+
     <p v-text="rating.firstName"/>
+
     <p v-text="rating.lastName"/>
+
     <p v-text="rating.profession"/>
+
     <p v-text="rating.phoneNumber"/>
+
     <div class="flex items-center gap-x-1">
       <img class="w-6 lg:w-7" :src="star" alt="rating star icon">
+
       <p>{{ rating.rating }}/5.0</p>
     </div>
+
     <router-link :to="`/user/listings/${rating.user_id}`" class="text-primary">დეტალურად</router-link>
   </div>
 </template>
