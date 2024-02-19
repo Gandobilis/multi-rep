efcde6edb8da94d86b5bbe84e57f95ac884fd772
Gandobilis <lashadeveloper@gmail.com>
2024-02-19 18:22:45 +0400

    Report.vue responsive.

--- src/components/modals/Report.vue
+++ src/components/modals/Report.vue
@@ -9,27 +9,28 @@ const emit = defineEmits(['closeModal'])
 <template>
   <form
       @submit.prevent="report(); emit('closeModal')"
-      class="absolute right-0 left z-10 m-auto flex lg:w-[40vw] flex-col gap-y-6 bg-white p-5 shadow-2xl">
+      class="absolute right-0 left z-10 m-auto flex max-lg:top-0 w-full lg:w-[40vw] flex-col gap-y-6 bg-white p-4 lg:p-5 shadow-2xl">
     <div class="flex w-full items-center justify-between border-b pb-6 border-border-gray">
-      <p class="text-2xl font-semibold">დაარეპორტეთ ეს პოსტი</p>
+      <p class="lg:text-2xl font-semibold">დაარეპორტეთ ეს პოსტი</p>
 
       <img @click="emit('closeModal')" class="cursor-pointer" src="/src/assets/icons/modals/close.svg"
            alt="close icon"/>
     </div>
 
-    <p>აირჩიეთ რეპორტის მიზეზი</p>
+    <p class="max-lg:text-sm">აირჩიეთ რეპორტის მიზეზი</p>
 
-    <div class="flex flex-col gap-y-7">
-      <div v-for="(row, index) in reportTypes" :key="index" class="flex justify-between">
+    <div class="flex flex-col gap-y-4 lg:gap-y-7">
+      <div v-for="(row, index) in reportTypes" :key="index" class="flex max-lg:flex-col max-lg:gap-y-4 justify-between">
         <div v-for="(item, itemIndex) in row" :key="itemIndex"
              @mouseover="hoveredIndex(index, itemIndex)"
              @mouseout="clearIndex"
              @click="clickedIndex(index, itemIndex)"
              :class="{'bg-primary text-white': (index.toString() + itemIndex.toString()) === _clickedIndex}"
-             class="flex items-center whitespace-nowrap rounded-md border px-5 gap-x-2.5 py-2.5 hover:bg-primary hover:cursor-pointer hover:text-white">
-          <p v-text="item"/>
+             class="flex items-center whitespace-nowrap rounded-sm lg:rounded-md border px-3 lg:px-5 gap-x-2.5 py-1.5 lg:py-2.5 hover:bg-primary hover:cursor-pointer hover:text-white">
+          <p class="max-lg:text-sm" v-text="item"/>
 
           <plus-icon
+              class="max-lg:w-3"
               :stroke="index.toString() + itemIndex.toString() === _hoverIndex || index.toString() + itemIndex.toString() === _clickedIndex ? 'white' : 'black'"
               :alt="item"
           />
@@ -39,7 +40,7 @@ const emit = defineEmits(['closeModal'])
 
     <div class="w-full flex justify-end font-medium mt-5">
       <button :class="{'!bg-primary !text-white': _clickedIndex !== ''}"
-              class="bg-gray-300 text-gray-500 px-10 py-2.5 rounded-md hover:shadow-2xl">
+              class="max-lg:text-xs w-full rounded-[5px] lg:rounded-md font-medium text-gray-500 transition bg-gray-300 py-2 lg:py-3.5 hover:shadow-xl">
         რეპორტი
       </button>
     </div>
